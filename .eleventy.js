056b77253abcc64453d1107cc5b1a945662bd4f7
Richard Birkby <rbirkby@gmail.com>
2024-03-31 12:09:30 +0100

    feat: Add footnote support to the 11ty markdown processor

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
+const markdownItFootnote = require("markdown-it-footnote");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("favicon.ico");
-};
 
+	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(markdownItFootnote));
+};
